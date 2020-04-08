427ab2274c9389ffe182ac582b22f484d2ad0346
springbottom <joshua.z.lin@berkeley.edu>
2020-04-08 02:37:24 -0700

    getting better

--- metropolis.c
+++ metropolis.c
@@ -3,59 +3,90 @@
 #include <stdlib.h>
 #include <math.h>
 #include <time.h>
-
-double rand_double() {
-  return rand()/(double)RAND_MAX;
-}
-
-double new_vector[2];
-
-double * update(FILE* fp, double beta, double old_vector[2], double neighbor[2]) {
-  double old_energy = old_vector[0]*neighbor[0]+old_vector[1]*neighbor[1];
-  double new_angle = 2*M_PI*rand_double();
-  new_vector[0] = cos(new_angle); new_vector[1] = sin(new_angle);
-  double new_energy = new_vector[0]*neighbor[0]+new_vector[1]*neighbor[1];
-
-  if (rand_double() <= exp(beta*(old_energy-new_energy))){
-    fprintf(fp,"%f",new_energy);
-    return new_vector;
-  }
-  fprintf(fp,"%f",0.0);
-  return old_vector;
-}
+#include "metropolis.h"
 
 
 int main(int argc, char *argv[]){
-  /*
-  ARGUMENTS:
-    -[1] : L, the lattice size. (we work on (2,N) lattice)
-    -[2] : INIT, the number of initial iterations.
-    -[3] : LOWER, the lower bound for beta
-    -[4] : UPPER, the upper bound for beta
-    -[5] : N, the number of beta points to sample.
-    -[6] : ITER, the number of iterations per lattice point, per beta.
-  */
 
   if(argc != 7){
     printf("Not the correct number of arguments.");
   }
 
   //Importing command line arguments?
-  int L = atoi(argv[1]);
-  int INIT = atoi(argv[2]);
-  char*pEND;
-  double LOWER = strtod(argv[3],&pEND);
-  double UPPER = strtod(argv[4],&pEND);
-  int N = atoi(argv[5]);
-  int ITER = atoi(argv[6]);
+  int L        = atoi(argv[1]);
+  int INIT     = atoi(argv[2]);
+  double LOWER = strtod(argv[3],NULL);
+  double UPPER = strtod(argv[4],NULL);
+  int N        = atoi(argv[5]);
+  int ITER     = atoi(argv[6]);
 
   //Setting up the file to print to?
-  FILE *fp;
   char file_str[200];
   sprintf(file_str,"%s_%s_%s_%s_%s_%s_%ld.txt",argv[1],argv[2],argv[3],argv[4],argv[5],argv[6],time(NULL));
   fp = fopen(file_str,"w");
 
+  //Actually doing things
+  init();
+  for (int i = 0; i < INIT){
+    MCMC_step(1/LOWER);
+  }
+  for (double inverse_beta = LOWER; inverse_beta <= UPPER; inverse_beta += (UPPER-LOWER)/N.){
+    fprintf(fp,"%f",inverse_beta);
+    for (int i = 0; i < ITER){
+      MCMC_step(1/inverse_beta);
+    }
+  }
+
   fprintf(fp,"test");
   fclose(fp);
   return 0;
+}
+
+
+
+double rand_double() {
+  return rand()/(double)RAND_MAX;
+}
+
+int init_state(){
+  for (int i = 0; i < 2; i++){
+    for (int j = 0; j < L; j++){
+      to_return[i][j][0] = cos(2*M_PI*rand_double());
+      to_return[i][j][1] = sin(2*M_PI*rand_double());
+    }
+  }
+  return 1;
+}
+
+int get_index(){
+  index[0] = rand()%2;
+  index[1] = rand()%2;
+
+  n1[0] = (index[0]+1)%2; n1[1] = index[1];
+  n2[0] = index[0]; n2[1] = (index[1]+1)%L;
+  return 1;
+}
+
+int MCMC_step(double beta){
+  get_index();
+
+  double old_vector[2] = state[index[0]][index[1]];
+  double vn1[2] = state[n1[0]][n1[1]];
+  double vn2[2] = state[n2[0]][n2[1]];
+  double neighbor[2] = {vn1[0]+vn2[0],vn1[1]+vn2[1]};
+
+  double old_energy = old_vector[0]*neighbor[0]+old_vector[1]*neighbor[1];
+  double new_angle = 2*M_PI*rand_double();
+  double new_vector[2] = {cos(new_angle), sin(new_angle)};
+  double new_energy = new_vector[0]*neighbor[0]+new_vector[1]*neighbor[1];
+
+  if (rand_double() <= exp(beta*(old_energy-new_energy))){
+    fprintf(fp,"%f,",new_energy);
+    state[index[0]][index[1]] = new_vector;
+    return 1;
+  }
+  fprintf(fp,"%f,",0.0);
+  return 1;
+
+  update()
 }
---
+++ metropolis.h
@@ -0,0 +1,22 @@
+
+
+//Command Line ARGUMENTS
+int L;         //the lattice size. (we work on (2,L) lattice)
+int INIT;      //the number of initial iterations.
+double LOWER;  //the lower bound for inverse beta
+double UPPER;  //the upper bound for inverse beta
+int N;         //the number of beta points to sample.
+int ITER;      //the number of iterations per lattice point, per beta.
+
+FILE *fp;
+int index[2];
+int n1[2];
+int n2[2];
+double[2][L][2] state;
+
+
+double rand_double();
+double * update(FILE* fp, double beta, double old_vector[2], double neighbor[2]);
+int init_state();
+int get_index();
+int MCMC_step();
